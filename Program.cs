87572c08e44967cbfade24def4df679de9137aa4
Harshithio <shettyharshithp@gmail.com>
2025-07-31 15:21:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine();          
+        int number = int.Parse(input);             
+
+        int square = number * number;
+        Console.WriteLine("Square of " + number + " is " + square);
+    }
+}
+
