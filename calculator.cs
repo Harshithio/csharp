b5e1f7577830511dc06fe3744bf9baaa2402ee85
Harshithio <shettyharshithp@gmail.com>
2025-08-13 10:13:51 +0530

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -1,16 +1,36 @@
 using System;
- class calculator
+class Calculator
 {
     static void Main()
     {
+        Console.WriteLine("Enter the number");
+        int op = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The operator number is" + op);
+
         Console.WriteLine("Enter two numbers");
         int a = Convert.ToInt32(Console.ReadLine());
         int b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Addition is" + (a + b));
-        Console.WriteLine("Subtraction is" + (a - b));
-        Console.WriteLine("Multiplication is" + (a * b));
-        Console.WriteLine("Division is" + (a / b));
-        Console.WriteLine("Modulus is" + (a % b));
-
+       
+        switch (op)
+        {
+            
+            case 1:
+                Console.WriteLine("Addition is" + (a + b));
+                break;
+            case 2:
+                Console.WriteLine("Subtraction is" + (a - b));
+                break;
+            case 3:
+                Console.WriteLine("Multiplication is" + (a * b));
+                break;
+            case 4:
+                Console.WriteLine("Division is" + (a / b));
+                break;
+            case 5:
+                Console.WriteLine("Modulus is" + (a % b));
+                break;
+            default:
+                Console.WriteLine("other calculation");
+                break;
+        }
     }
-}
